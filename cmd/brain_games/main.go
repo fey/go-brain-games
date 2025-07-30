51df2eed5bd8bf52d6bc37b3bc6a76ed8174d0d4
Nikolay Gagarinov <feycot@gmail.com>
2025-07-30 22:10:07 +0500

    add question

--- cmd/brain_games/main.go
+++ cmd/brain_games/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+    import (
+        "bufio"
+        "fmt"
+        "os"
+    )
+// import (
+// 	"fmt"
+
+// 	"github.com/manifoldco/promptui"
+// )
+
+// func main() {
+// 	prmpt := (promptui.Prompt{
+// 		Label: "Mah I hamve your name?",
+// 	})
+// 	name, _ := prmpt.Run()
+
+// 	fmt.Printf("Hello, %s!\n", name)
+// }
 
 func main() {
 	fmt.Println("Welcome to the Brain Games!")
+	name := Question("May i have your name? ")
+
+	fmt.Printf("Hello, %s\n", name)
+}
+
+func Question(query string) string {
+	fmt.Print(query)
+	reader := bufio.NewReader(os.Stdin)
+	answer, _ := reader.ReadString('\n')
+
+	return answer
 }
